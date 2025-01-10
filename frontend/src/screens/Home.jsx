e448bd6293c4f084f1983130d813802c4b533622
karannkushwaha <karankushwaha7227@gmail.com>
2025-01-10 21:27:39 +0530

    create project view

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,9 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../context/user.context";
+import axiosInstance from "../config/axios";
 
 const Home = () => {
   const { user } = useContext(UserContext);
-  return <div>{JSON.stringify(user)}</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [projectName, setprojectName] = useState(null);
+  const createProject = (e) => {
+    e.preventDefault();
+
+    axiosInstance
+      .post("/project/create", { name: projectName })
+      .then((res) => {
+        setIsModalOpen(false);
+        setprojectName(null);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <main className="p-4">
+      <div className="projects">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="project p-4 border border-slate-300 rounded-md"
+        >
+          New Project
+          <i className="ri-link ml-2"></i>
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-md shadow-md w-1/3">
+            <h2 className="text-xl mb-4">Create New Project</h2>
+            <form onSubmit={createProject}>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Project Name
+                </label>
+                <input
+                  onChange={(e) => setprojectName(e.target.value)}
+                  value={projectName}
+                  type="text"
+                  className="mt-1 block w-full border p-2 border-slate-300 rounded-md shadow-sm focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                  required
+                />
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  className="mr-2 px-4 py-2 bg-gray-300 rounded-md"
+                  onClick={() => setIsModalOpen(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-blue-600 text-white rounded-md"
+                >
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Home;
